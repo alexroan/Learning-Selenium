58300553dc0c36c3b3c461bd499678b8160b90a6
Alex Roan <alex.roan@hotmail.com>
2017-10-05 19:45:42 +0100

    Added unittest and Tripsi form fill python files@

--- python_org_search.py
+++ python_org_search.py
@@ -1,12 +1,23 @@
 from selenium import webdriver
+# Keys
 from selenium.webdriver.common.keys import Keys
 
+# Create instance of Chrome
 driver = webdriver.Chrome()
+# Go to a web page
 driver.get("http://www.python.org")
+# Assertion that the title of the page has 'Python' in it
 assert "Python" in driver.title
+# Many find_element_by_*() methods
+# Name is one of them
 elem = driver.find_element_by_name("q")
+# Clear any text in the input field
 elem.clear()
+# Populate input
 elem.send_keys("pycon")
+# Enter
 elem.send_keys(Keys.RETURN)
+# Assert if no results are found
 assert "No results found." not in driver.page_source
-driver.close()
+# Close the driver
+ driver.close()
---
+++ tripsi_form_fill.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+
+driver = webdriver.Chrome()
+driver.get('https://tripsi.io')
+assert "Tripsi" in driver.title
+# Enter search airport
+leave_from_input = driver.find_element_by_id('leaveFrom')
+leave_from_input.clear()
+leave_from_input.send_keys('Cardiff')
+# Select currency
+currency_select = Select(driver.find_element_by_id('currencySelect'))
+currency_select.select_by_value('EUR')
+# Click go
+search_button = driver.find_element_by_id('searchbutton')
+search_button.click()
