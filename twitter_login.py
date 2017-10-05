247297c4b978492566bafcdf9dba271d06ec1742
Alex Roan <alex.roan@hotmail.com>
2017-10-05 20:36:36 +0100

    Added comments to twitter login

--- twitter_login.py
+++ twitter_login.py
@@ -6,19 +6,24 @@ import getpass
 driver = webdriver.Chrome()
 driver.get('https://twitter.com')
 
+# Open the login dialog
 open_dialog_btn = driver.find_element_by_class_name('js-login')
 open_dialog_btn.click()
 
+# Get and enter username
 username = input('Twitter username:')
 username_input = driver.find_element_by_class_name('js-signin-email')
 username_input.send_keys(username)
 
+# Get and enter password
 password = getpass.getpass('Password:')
 password_input = driver.find_element_by_xpath("//input[@type='password']")
 password_input.send_keys(password)
 
+# Click login
 submit_button = driver.find_element_by_class_name('js-submit')
 submit_button.click()
 
+# Wait and close the driver
 ui.WebDriverWait(driver, 25)
 driver.close()
